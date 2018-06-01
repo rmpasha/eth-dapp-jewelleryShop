dbafd3f7fa1567d1933973de6b72ce54b3e66173
Rajendra Maharjan <rm_pasha@hotmail.com>
2018-06-01 18:42:10 -0400

    login eth address
    Display current login eth address and balance of the user

--- src/js/app.js
+++ src/js/app.js
@@ -16,7 +16,6 @@ App = {
         itemTemplate.find('.item-price').text(data[i].price);
         itemTemplate.find('.item-material').text(data[i].material);
         itemTemplate.find('.btn-purchase').attr('data-id', data[i].id);
-
         itemsRow.append(itemTemplate.html());
       }
     });
@@ -27,13 +26,86 @@ App = {
   initWeb3: function() {
     //Connecting to Network provider
     if(typeof web3 !== 'undefined') {
+      //MetaMask connects from this block
       App.web3Provider = web3.currentProvider;
+      console.log("Automatically picked Provider: " + App.web3Provider);
     } else {
-      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+      //Forcing to connect Local Network if not defined from above block
+      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:8545');
+      console.log("Direct localhost Provider:" + App.web3Provider);
     }
 
     web3 = new Web3(App.web3Provider);
 
+    //Get web3 version
+    var version = web3.version.api;
+    console.log("Web3 API version : " + version); // "0.20.3"
+
+////////////////////////////Listening for selected account change//////////////
+var curAccount = web3.eth.accounts[0];
+var accountInterval = setInterval(function() {
+  if(web3.eth.accounts[0] !== curAccount) {
+    curAccount=web3.eth.accounts[0];
+    updateInterface();
+  }
+},100);
+/////////////////////////////////////////////////////////////////////////////
+
+    /////////////////// To know Which Network Connected From MataMask //////////////////////
+    web3.version.getNetwork((err,netId) => {
+      switch (netId) {
+        case "1":
+          console.log('This is mainnet' + netId);
+          break;
+        case "2":
+          console.log('This is the deprecated Morden test network.'+ netId);
+          break;
+        case "3":
+          console.log('This is the ropsten test network');
+          break;
+        case "1527864774980":
+          console.log('This is local network');
+          break;
+        default:
+          console.log('This is an unknown network '+ netId);
+      }
+    });
+    ////////////////////////////////////////////////////////////////////////////////////////
+
+    ////////////////////////////////////Get MetaMask Current Address///////////////////////
+    web3.eth.getAccounts(function(error, accounts) {
+      if(error) {
+        console.log("Getting Account: " + error);
+      }
+      else {
+        var account = accounts[0];
+        $('#ethAddress').text(account);
+      }
+    ///////////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////Get Current Balance///////////////////////////////////////
+      //var balance = web3.fromWei(web3.eth.getBalance(web3.eth.accounts[0]));
+      //Above commented line doesn't work here, we need to use synchronous callback
+      //as below to get the balance of MetaMask Wallet Address
+      web3.eth.getBalance(account, function(error, result){
+        if(!error)  {
+          //console.log("Balance: " + result/1000000000000000000);
+          $('#ethBalance').text(result/1000000000000000000);
+        }
+        else
+          console.log("Error: " + error);
+      });
+    });
+  ////////////////////////////////////////////////////////////////////////////////////////
+
+  ////////////////////////////Get Block if need////////////////////////////////////////
+  /*  web3.eth.getBlock(4, function(error, result){
+    if(!error)
+        console.log(JSON.stringify(result));
+    else
+        console.error("GetBlock Error: " + error);
+  });
+  */
+  //////////////////////////////////////////////////////////////////////////////////////////////
     return App.initContract();
   },
 
@@ -58,8 +130,9 @@ App = {
 
      App.contracts.PurchaseJewellery.deployed().then(function(instance) {
        purchaseInstance = instance;
+
 //getAllBuyers return as view, use call method
-       return purchaseInstance.getAllBuyers.call(); 
+       return purchaseInstance.getAllBuyers.call();
      }).then(function(buyers){
        for(i=0;i<buyers.length;i++){
          if(buyers[i] !== '0x0000000000000000000000000000000000000000') {
@@ -82,7 +155,7 @@ App = {
       itemPrice=data[itemId].price;
        //console.log("Price from json: " + data[itemId].price);
     //});
-    
+
     var purchaseInstance;
     web3.eth.getAccounts(function(error, accounts) {
       if(error) {
